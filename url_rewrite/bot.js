add1ca349815fc1afc47cc115329fa8389a86177
iammegaladon <127837255+iammegaladon@users.noreply.github.com>
2023-12-30 11:57:38 +0500

    Update bot.js

--- url_rewrite/bot.js
+++ url_rewrite/bot.js
@@ -1,5 +1,5 @@
 // Check if the current URL is '/index.html' and replace it with '/home'
 if (window.location.pathname === '/megai-bot.html') {
-  const newUrl = window.location.href.replace('/megai-bot.html', '/megai-bot');
+  const newUrl = window.location.href.replace('/megai-bot.html', '/megai/chat');
   window.history.replaceState({}, document.title, newUrl);
 }
