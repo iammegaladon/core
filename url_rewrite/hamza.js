05c0ef058a64b46bc64d7c66edd10907fa5f68b4
iammegaladon <127837255+iammegaladon@users.noreply.github.com>
2024-01-02 02:00:43 +0500

    Update hamza.js

--- url_rewrite/hamza.js
+++ url_rewrite/hamza.js
@@ -1,4 +1,4 @@
 if (window.location.pathname === '/hamza.html') {
-  const newUrl = window.location.href.replace('/hamza.html', '/founder/hamza');
+  const newUrl = window.location.href.replace('/hamza.html', '/Team');
   window.history.replaceState({}, document.title, newUrl);
 }
