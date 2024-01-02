a9817f38048a1ce3c0f57e6ac04aaf6890c63cfb
iammegaladon <127837255+iammegaladon@users.noreply.github.com>
2024-01-02 07:25:27 +0500

    Update security_check.js

--- other_js/security_check.js
+++ other_js/security_check.js
@@ -1,17 +1,3 @@
-const isChromium = window.chrome !== undefined;
 
-if (isChromium) {
-    // Check if not already redirected
-    if (!window.location.href.includes("index.html")) {
-        // Redirect to index.html after 3 seconds
-        setTimeout(() => {
-            window.location.href = "index.html";
-        }, 3000);
-    }
-} else {
-    // Check if not already redirected
-    if (!window.location.href.includes("security.html")) {
-        // Redirect to security.html immediately
-        window.location.href = "security.html";
     }
 }
